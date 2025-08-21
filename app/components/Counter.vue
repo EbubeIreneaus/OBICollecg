4e841096c86809c13c25e4c3b0ca5c1ce879f85d
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2025-08-22 05:32:32 +0800

    Animated Counter plugins added

--- app/components/Counter.vue
+++ app/components/Counter.vue
@@ -1,41 +1,33 @@
 <template>
   <div class="grid md:grid-cols-2 gap-3 mt-5">
     <u-card :ui="{ root: 'bg-amber-500 ring-0' }">
-      <count-up
-      :options="countOptions"
-        :endVal="100"
-        class="text-2xl font-arial font-extrabold text-center"
-      />
+      <div class="text-2xl font-arial font-extrabold text-center">
+        <AnimatedCounter :value="100" class="inline"/>+
+      </div>
       <p class="text-xl font-playfair font-bold text-center mt-3">
         Skilled Teachers
       </p>
     </u-card>
     <u-card variant="soft" :ui="{ root: ' ring-0' }">
-      <count-up
-        :end-val="200"
-        :options="countOptions"
-        class="text-2xl font-arial font-extrabold text-center"
-      />
+       <div class="text-2xl font-arial font-extrabold text-center">
+        <AnimatedCounter :value="200" class="inline"/>+
+      </div>
       <p class="text-xl font-playfair font-bold text-center mt-3">
         Equiped Classroom
       </p>
     </u-card>
     <u-card variant="soft" :ui="{ root: 'bg-primary ring-0' }">
-      <count-up
-        :end-val="30000"
-        :options="countOptions"
-        class="text-2xl font-arial font-extrabold text-center"
-      />
+       <div class="text-2xl font-arial font-extrabold text-center">
+        <AnimatedCounter :value="1000" :duration="1" class="inline"/>+
+      </div>
       <p class="text-xl font-playfair font-bold text-center mt-3 text-white">
         Graduated Student
       </p>
     </u-card>
     <u-card variant="soft" :ui="{ root: 'bg-green-500 ring-0' }">
-      <count-up
-        :end-val="500"
-        :options="countOptions"
-        class="text-2xl font-arial font-extrabold text-center"
-      />
+       <div class="text-2xl font-arial font-extrabold text-center">
+        <AnimatedCounter :value="200" class="inline"/>+
+      </div>
       <p class="text-xl font-playfair font-bold text-center mt-3">
         Online Course
       </p>
@@ -44,7 +36,6 @@
 </template>
 
 <script setup>
-import CountUp from 'vue-countup-v3'
 const countOptions = {
   suffix: "+",
   enableScrollSpy: true,
